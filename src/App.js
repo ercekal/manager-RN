2c9f58dcdb1cec02c6960f9643d9690fb9899aa0
ercekal <ercekalabalikoglu@gmail.com>
2017-01-16 16:05:52 +0000

    does navigation with Scenes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import ReduxThunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux' ;
 import reducers from './reducers';
 import firebase from 'firebase';
-import LoginForm from './components/LoginForm';
-
+import Router from './Router';
 
 class App extends Component {
   componentWillMount() {
@@ -23,7 +22,7 @@ class App extends Component {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
       <Provider store={store} >
-        <LoginForm />
+        <Router />
       </Provider>
     );
   }
